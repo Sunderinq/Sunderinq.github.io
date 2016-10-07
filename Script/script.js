5f4be086284c0b2f278984968c3bd7ac51287e76
Sunderinq <tegnoor.gill@hotmail.com>
2016-10-07 14:20:06 -0700

    Create script.js

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+var today = new Date();
+var hourNow = tpday.getHours();
+var greeting;
+
+if (hourNow > 18) {
+  greeting = ' Good Evening!';
+  } else if (hourNow > 12) {
+  greeting = ' Good afternoon!';
+  } else if (hourNow > 0){
+  greeting = ' Welcome!";
+  }
